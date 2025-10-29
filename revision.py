35427568e886ee7e169338aca6b3616a23f4f7ae
piyushkumawat1 <piyushkumawat3440@gmail.com>
2025-10-30 02:42:07 +0530

    i did loop question

--- revision.py
+++ revision.py
@@ -29,7 +29,7 @@ idx = 0
 while idx <= len(heroes)-1 :
     print(heroes[idx],end = "   ")
     idx+=1'''
-tup = (1,4,9,16,25,36,49,64,81,100)
+'''tup = (1,4,9,16,25,36,49,64,81,100)
 idx = 0
 x = int(input("Give the number :"))
 while idx <len(tup) :
@@ -38,4 +38,27 @@ while idx <len(tup) :
         break
     else :
         print("finding")   
-    idx+=1     
+    idx+=1  '''
+'''list = [1,4,9,16,25,36,49,64,81,100,49]
+x = 49
+idx = 0
+for i in list  :
+    if(i == x) :
+       print("number founded at idx",idx)
+       
+    idx+=1'''
+'''n = int(input("Enter number : "))
+for i in range(1,11):
+    print(n*i)'''
+'''n = int(input("Enter number : "))
+i=1 
+fact =1
+while i<=n :
+    fact = fact *i
+    i+=1
+print("factorial of ",n,"is =",fact)  '''
+n = int(input("Enter number : "))
+fact =1
+for i in range(1,n+1):
+    fact =fact*i
+print("factorial",n,"=",fact)
