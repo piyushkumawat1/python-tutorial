900fc947557b6ddfd1117c0ad623a8d8a050abbf
piyushkumawat1 <piyushkumawat3440@gmail.com>
2025-11-21 20:03:15 +0530

    100 day of challenge

--- oops/revision.py
+++ oops/revision.py
@@ -32,50 +32,57 @@
 
 
 
-class Account :
-    def __init__(self,acc,balance):
-        self.acc = acc
-        self.balance = balance
-    @staticmethod
-    def welcome():
-        print("Welcome to HDFC bank ")
+# class Account :
+#     def __init__(self,acc,balance):
+#         self.acc = acc
+#         self.balance = balance
+#     @staticmethod
+#     def welcome():
+#         print("Welcome to HDFC bank ")
 
 
-    def credit(self,amount) :
-        self.balance+= amount
-        print(f"{amount} was credited")
+#     def credit(self,amount) :
+#         self.balance+= amount
+#         print(f"{amount} was credited")
 
-    def debit(self,amount) :
-        self.balance-=amount
-        print(f"{amount} was debited")  
+#     def debit(self,amount) :
+#         self.balance-=amount
+#         print(f"{amount} was debited")  
 
-    def get_balance(self):
-        return self.balance
+#     def get_balance(self):
+#         return self.balance
     
         
-cus1 = Account(6372,10000)
-cus1.welcome()
-cus1.credit(1000)
-cus1.debit(2354)
+# cus1 = Account(6372,10000)
+# cus1.welcome()
+# cus1.credit(1000)
+# cus1.debit(2354)
 
-print("Final balance :",cus1.get_balance())
-
-
-class Order :
-    def __init__(self,item ,price):
-        self.item = item
-        self.price = price
-
-    def __gt__(self,ord2) :
-        return self.price > ord2.price
+# print("Final balance :",cus1.get_balance())
 
 
+# class Order :
+#     def __init__(self,item ,price):
+#         self.item = item
+#         self.price = price
 
-ord1 = Order("Tea",10)
-ord2 = Order("samosa",25)
+#     def __gt__(self,ord2) :
+#         return self.price > ord2.price
 
 
-print(ord1.price<ord2.price)
+
+# ord1 = Order("Tea",10)
+# ord2 = Order("samosa",25)
+
+
+# print(ord1.price<ord2.price)
+# def 
 
 
      
+def show(n) :
+        while n != 0 :
+          print("Hello guys ")
+          return show(n-1)
+n = int(input("Enter the times : "))
+show(n)    
---
+++ oops/revisiop.py/r.py
@@ -0,0 +1,14 @@
+#Day 1 - Intrdoduction
+#Day 2 - What is python and how to install 
+#Day 3 - what is modulas and library 
+#Day 4 - indroduction of programme which will make 
+#Day 5 - Comments & Esacape Squences ,print statement 
+#print("Hy guys Welcome to the Day 5 ", sep = "  ", end = " ?") # default sep = " ", end = \n
+#Day 6 - data types and variables
+#Day 7 - make a calculator using pyhton operator
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+add = num1 + num2
+multiple  = num1 * num2
+Divison = num1 /num2
+Modulaos = num1 % num2
