1754f9b31aee6d234ec16c464b21fefb4024850e
piyushkumawat1 <piyushkumawat3440@gmail.com>
2025-12-07 20:54:23 +0530

    make a codder

--- oops/logs.py
+++ oops/logs.py
@@ -150,23 +150,143 @@
 # print(Student.name)
 
 
-class Complex :
-    def __init__(self,real,img):
-        self.real = real
-        self.img = img
+# class Complex :
+#     def __init__(self,real,img):
+#         self.real = real
+#         self.img = img
 
-    def showNum(self) :
-        print(self.real,"i","+",self.img,"j")
+#     def showNum(self) :
+#         print(self.real,"i","+",self.img,"j")
 
-    def __add__(self,num2) :
-        newreal = self.real + num2.real    
-        newimg = self.img + num2.img
-        return Complex(newreal,newimg)
+#     def __add__(self,num2) :
+#         newreal = self.real + num2.real    
+#         newimg = self.img + num2.img
+#         return Complex(newreal,newimg)
 
 
-num1 = Complex(1,3)
-num1.showNum()  
-num2 = Complex(2,4)
-num2.showNum()  
-num3 = num1 + num2 
-num3.showNum()     
+# num1 = Complex(1,3)
+# num1.showNum()  
+# num2 = Complex(2,4)
+# num2.showNum()  
+# num3 = num1 + num2 
+# num3.showNum()     
+
+# class Circle : 
+#     def __init__(self,radius) :
+#         self.radius = radius
+
+
+#     def area(self) :
+#         return 3.14 *self.radius**2 
+
+#     def perimeter(self) :
+#         return 3.14*2*self.radius  
+
+# n = float(input("Enter the radius of circle : "))  
+
+# c1 = Circle(n)
+# print("Area of circle : ",c1.area())
+# print("perimeter of circle",c1.perimeter())  
+# class Employee :
+#     def __init__(self,dep,role,salary) :
+#         self.dep = dep 
+#         self.role = role
+#         self.salary = salary
+
+#     def showDetails(self) :
+#         print("role :",self.role)
+#         print("department:",self.dep)
+#         print("salary :",self.salary)
+# Emp1 = Employee("Software","cloud engineer",100000)
+
+
+# class Engineer(Employee) :
+#     def __init__(self,name,age) :
+#         self.name = name
+#         self.age = age
+#         super().__init__("Software","cloud engineer",100000)
+#     def showDetails(self) :
+#         print("Name :",self.name)
+#         print("age : ",self.age)
+#         super().showDetails()
+# Emp = Engineer("Piyush kumawat",21)
+# Emp.showDetails()
+# import random
+# target = random.randint(1,100)
+# while True :
+#     userChoice = input("Enter the Choice or Quit(Q) :")
+#     if userChoice=='Q' :
+#         break
+#     userChoice = int(userChoice)
+#     if userChoice == target : 
+#         print("Congrats you guess correct")
+#         print("-------GAME OVER------") 
+#     elif userChoice > target :
+#         print("Your number was to big please choose small number  ")
+#     else :
+#         print("your choice was too small guess big one ")
+
+# import random 
+# import string 
+# pass_len = 4
+# password = ""
+# values = string.ascii_letters + string.digits + string.punctuation
+# for i in range(pass_len) :
+#     password += random.choice(values)
+# print(password)
+
+
+
+
+
+
+
+
+
+
+
+# import random
+# import string
+# pasa_len = 6
+# values = string.ascii_letters + string.digits + string.punctuation
+# password = ""
+# for i in range(pasa_len) :
+#   password += random.choice(values)
+# print(password)
+# def Try(count = 1) :
+#     pass_lock = input("Enter the password  :")
+#     if password == pass_lock : 
+#      print("Device Unlocked ")
+#     else : 
+#       print("Wrong password Please Try again ")
+#       if count == 3 : 
+#         return 
+#       else : 
+#         Try(count+1) 
+        
+# Try()        
+        
+
+import random
+
+choices = ["rock" ,"paper", "sessor"]
+
+while True : 
+     computer  = random.choice(choices)
+     userchoice = input("Choose (rock ,paper,sessor or quit) : ").lower()
+     print("Computer choice ",computer)
+     if userchoice == "quit" : 
+       print("GAME OVER ")
+       break
+     elif computer == userchoice : 
+         print("Tie ")
+     elif userchoice not in choices :
+         print("Invalid choice")     
+     elif ((computer == "rock" and userchoice == 'sessor') \
+           or (computer == "paper" and userchoice == "rock") \
+            or computer == 'sessor' and userchoice == "paper" ) :
+         print("You lose")
+     else : 
+         print("You win")   
+ 
+  
